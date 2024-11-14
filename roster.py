4e2d9735fea4f7f296ae5cc5e7992fc74372dbcb
katmoran <kmoran@unc.edu>
2024-11-14 11:41:04 -0500

    expanded dictionary

--- roster.py
+++ roster.py
@@ -2,6 +2,9 @@
 import pandas as pd 
 
 roster = ["Davis","Cadeau", "Trimble"]
-player = {"Last Name": roster}
-data = pd.DataFrame(player)
-print(data)
+player = {"Last Name": roster, 
+          "First Name": ["RJ", "Elliot", "Seth"],
+          "height": [72, 73, 75], 
+          "weight":[180, 180, 195]}
+data= pd.DataFrame(player)
+print(data)          
